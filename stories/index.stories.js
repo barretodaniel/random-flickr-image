0c6d78e8935fe3744bb5f4e04ae92b30e3147a22
Daniel Barreto <daniel.barreto717@gmail.com>
2019-07-24 17:06:32 -0400

    more storybook stories and source addon

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import RandomFlickrImage from '../src/RandomFlickrImage';
+import RandomFlickrImage, { Sizes } from '../src/RandomFlickrImage';
 
-storiesOf('RandomFlickrImage', module).add('with defaults', () => <RandomFlickrImage />);
+storiesOf('RandomFlickrImage', module)
+    .add('with defaults (3 seconds)', () => <RandomFlickrImage />)
+    .add('with interval set to 5 seconds', () => <RandomFlickrImage interval={5} />)
+    .add('with a set size', () => <RandomFlickrImage size={Sizes.LargeSquare} />)
+    .add('adding styles', () => (
+        // you can also pass `className` to override styles
+        <RandomFlickrImage
+            style={{ border: '1px solid #CCC', padding: 8, borderRadius: 150 }}
+            size={Sizes.LargeSquare}
+        />
+    ));
